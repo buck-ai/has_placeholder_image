6b5bd7658065aa6b83b8e1218057ec3e9faca970
Muhammet Dilmaç <iletisim@muhammetdilmac.com.tr>
2020-07-05 16:02:45 +0300

    Added new tests

--- test/has_placeholder_image/active_record_test.rb
+++ test/has_placeholder_image/active_record_test.rb
@@ -12,16 +12,6 @@ module HasPlaceholderImage
       assert_includes Human.singleton_methods, :has_placeholder_image
     end
 
-    test 'has_placeholder_image_options is exist?' do
-      assert_includes Human.instance_methods, :placeholder_image_options
-    end
-
-    test 'has_placeholder_image_options has default values?' do
-      human = Human.new
-      human.name = 'Foo Bar'
-      assert_equal human.placeholder_image_options, HasPlaceholderImage.default_options
-    end
-
     test "If don't have image create placeholder" do
       human = Human.new
       human.name = 'Foo Bar'
@@ -36,6 +26,19 @@ module HasPlaceholderImage
       company.save
 
       assert_equal company.logo.attached?, true
+    end
+
+    test 'Human module options are default?' do
+      assert_equal Human.placeholder_image_options, HasPlaceholderImage.default_options
+    end
+
+    test 'Company module options are modified?' do
+      default_options = HasPlaceholderImage.default_options
+      merged_options = default_options.update(source: :title, target: :logo,
+                                              background_color: '#335eea',
+                                              font_size: 200)
+
+      assert_equal Company.placeholder_image_options, merged_options
     end
   end
 end
