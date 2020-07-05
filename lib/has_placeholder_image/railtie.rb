11c439b422759399616d28d712bfc24acb1097c9
Muhammet Dilmaç <iletisim@muhammetdilmac.com.tr>
2020-07-05 16:02:53 +0300

    Updated railtie

--- lib/has_placeholder_image/railtie.rb
+++ lib/has_placeholder_image/railtie.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
+require 'active_record/railtie'
+
 module HasPlaceholderImage
   class Railtie < ::Rails::Railtie
     initializer 'has_placeholder_image.initialize' do |app|
       railtie_collection = app.railties
       activerecord_railtie = railtie_collection.select { |railtie| railtie.class.to_s == 'ActiveRecord::Railtie' }
-      return unless activerecord_railtie
-
-      require 'has_placeholder_image/active_record'
-      ::ActiveRecord::Base.include(HasPlaceholderImage::ActiveRecord)
+      ::ActiveRecord::Base.include(HasPlaceholderImage::ActiveRecord) if activerecord_railtie
     end
   end
 end
