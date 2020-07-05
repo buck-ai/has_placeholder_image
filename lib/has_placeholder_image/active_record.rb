7a510e7b9cf3dcec4b17e6c76cca5e121ca1c79b
Muhammet Dilmaç <iletisim@muhammetdilmac.com.tr>
2020-07-05 16:01:57 +0300

    Added new active record module

--- lib/has_placeholder_image/active_record.rb
+++ lib/has_placeholder_image/active_record.rb
@@ -2,22 +2,29 @@
 
 module HasPlaceholderImage
   module ActiveRecord
-    extend ActiveSupport::Concern
+    def self.included(base)
+      base.extend ClassMethods
+    end
 
-    class_methods do
+    module ClassMethods
       def has_placeholder_image(**options)
-        @@placeholder_image_options = HasPlaceholderImage.default_options.merge(options)
+        default_options = HasPlaceholderImage.default_options
+        merged_options = default_options.update(options)
+
         mattr_accessor :placeholder_image_options
+        self.placeholder_image_options = merged_options
 
         after_validation :generate_placeholder_image, if: :need_placeholder?
+
+        include HasPlaceholderImage::ActiveRecord::InstanceMethods
       end
     end
 
-    included do
+    module InstanceMethods
       private
 
       def generate_placeholder_image
-        options = @placeholder_image_options
+        options = self.class.placeholder_image_options
         text = HasPlaceholderImage::TextGenerator.send(options[:transformer],
                                                        @placeholder_image_source)
         image = HasPlaceholderImage::ImageGenerator.new(text, **options)
@@ -26,9 +33,8 @@ module HasPlaceholderImage
       end
 
       def need_placeholder?
-        @placeholder_image_options = self.class.placeholder_image_options
-        @placeholder_image_target = send(@placeholder_image_options[:target])
-        @placeholder_image_source = send(@placeholder_image_options[:source])
+        @placeholder_image_target = send(self.class.placeholder_image_options[:target])
+        @placeholder_image_source = send(self.class.placeholder_image_options[:source])
 
         !@placeholder_image_target.attached? && !@placeholder_image_source.blank?
       end
